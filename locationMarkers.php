3817561a3b7cb4be0daa5566c68785d87863cde0
garbwalk <garbwalk@ut.utm.edu>
2017-04-17 18:27:08 -0500

    Deleted useless file, removed some debug info from frontpage, added redirect

--- locationMarkers.php
+++ locationMarkers.php
@@ -3,29 +3,31 @@ require_once('../phplogin.php');
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-	//This file takes all current check-ins and outputs them as a JSON formatted list
-	$sql = mysqli_query($conn,"SELECT * FROM geolocate");
-	$total = mysqli_fetch_assoc(mysqli_query($conn,"SELECT COUNT(*) as total FROM geolocate"));
-	$start = 0;
-	echo "[";
-	foreach ($sql as $user) {
-    echo "{\"Title\":\"$user[Username]\",
-		 \"latitude\":\"$user[Lat]\",
-    	 \"longitude\":\"$user[Log]\",
-	 \"Image\":\"$user[Image]\",
-	 \"Time\":\"$user[Time]\"";
-	echo '}';
-		if(($start+1)<$total['total']){
-			echo ",";
-		}
-	$start = $start + 1;
+//This file takes all current check-ins and outputs them as a JSON formatted list
+$sql = mysqli_query($conn,"SELECT * FROM geolocate");
+$total = mysqli_fetch_assoc(mysqli_query($conn,"SELECT COUNT(*) as total FROM geolocate"));
+$start = 0;
+echo "[";
+foreach ($sql as $user) {
+	echo "{\"Title\":\"$user[Username]\",
+		\"latitude\":\"$user[Lat]\",
+		\"longitude\":\"$user[Log]\",
+		\"Image\":\"$user[Image]\",
+		\"Time\":\"$user[Time]\"";
+		echo '}';
+	if(($start+1)<$total['total']){
+		echo ",";
 	}
-	echo "]";
+	$start = $start + 1;
+}
+
+echo "]";
 
 
 
--- updateLocation.php
+++ updateLocation.php
@@ -1,34 +1,34 @@
 <?php
-//Garrett Walker
-//Table Assignment 1
 require_once('../phplogin.php');
+
+
+//Redirect
+echo '<meta http-equiv="refresh" content="0; url=./" />';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-if(isset($_POST['submit']))
-{
+if(isset($_POST['submit'])){
 	echo "TEST";
 	$username 	= $_POST['Username'];
 	$fullname 	= $_POST['FullName'];
 	$lat 		= $_POST['Lat'];
 	$long 		= $_POST['Long'];
-	//$timestamp 	= $_POST[''];
 	$imageURL 	= $_POST['ImageURL'];
 
-	echo $username;
-	echo $fullname;
-	echo $lat;
-	echo $long;
-	echo $imageURL;
+	//echo $username;
+	//echo $fullname;
+	//echo $lat;
+	//echo $long;
+	//echo $imageURL;
 
 
-	if($username!='NULL')
-	{
+	if($username!='NULL'){
 		mysqli_query($conn,"DELETE FROM geolocate WHERE Username='$username'");
 		mysqli_query($conn,"INSERT INTO geolocate (Username, FullName, Lat, Log, Image) VALUES  ('$username','$fullname','$lat','$long','$imageURL')");
 	}
